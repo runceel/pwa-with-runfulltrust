0a1f5a8d786492df6960c824db14663029135cc9
Kazuki Ota <kaota@microsoft.com>
2018-09-20 00:40:23 +0900

    Add project files.

---
+++ PWA.RunFullTrust/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PWA.RunFullTrust.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
